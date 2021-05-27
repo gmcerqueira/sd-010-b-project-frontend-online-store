e2fdbc47f54375a3b560f201ea30d49ccad1783d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-27 16:19:38 -0300

    creat section with categories

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import * as api from './services/api';
 
-function App() {
-  return (
-    <Router>
-      <Route path="/cart" component={ Cart } />
-      <Route exact path="/" component={ Home } />
-    </Router>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories = async () => {
+    const categories = await api.getCategories();
+
+    this.setState({ categories });
+  }
+
+  rendersRoutes = () => {
+    const { categories } = this.state;
+    return (
+      <Router>
+        <Switch>
+          <Route path="/cart" component={ Cart } />
+          <Route exact path="/" render={ () => <Home categories={ categories } /> } />
+        </Switch>
+      </Router>
+    );
+  }
+
+  render() {
+    return (
+      <div>{this.rendersRoutes()}</div>
+    );
+  }
 }
-
-export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
+    const { categories } = this.props;
     return (
       <div>
         <Link to="/cart" data-testid="shopping-cart-button">
@@ -11,6 +12,13 @@ export default class Home extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <section>
+          {categories.map(({ name }, i) => (
+            <p key={ i } data-testid="category">
+              {name}
+            </p>
+          ))}
+        </section>
       </div>
     );
   }
