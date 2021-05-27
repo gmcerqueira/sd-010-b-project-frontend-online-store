cfa413c2af50315cb25724bc1b88aac1fe19a331
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-27 16:09:01 -0300

    creat home page with message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import * as api from './services/api';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      {console.log(api.getCategories())}
-    </div>
+    <Router>
+      <Route path="/" component={ Home } />
+    </Router>
   );
 }
 
