c657520f8ecbbcf3921b80ff006b0e1cbde3757d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-27 16:09:26 -0300

    creat cart page with message

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
-      <Route path="/" component={ Home } />
+      <Route path="/cart" component={ Cart } />
+      <Route exact path="/" component={ Home } />
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          cart
+        </Link>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
     );
   }
 }
