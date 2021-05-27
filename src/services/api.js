fa1bfbcea9ff95d134c88f7a56167ef1a333cc79
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-27 15:30:09 -0300

    implemetation of api access module

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 export async function getCategories() {
-  // Implemente aqui
+  const URL_API = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch(URL_API);
+  const response = await request.json();
+
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const URL_API = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const request = await fetch(URL_API);
+  const response = await request.json();
+
+  return response;
 }
