e46d21c04dc4fdc270a7a806aa72e3f93fe078da
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-27 21:41:13 -0300

    add cart link on product details

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import * as auxApi from '../services/auxApi';
 
 export default class ProductDetails extends Component {
@@ -24,19 +25,24 @@ export default class ProductDetails extends Component {
     const { addToCart } = this.props;
     const { product } = this.state;
     return (
-      <section>
-        <h3 data-testid="product-detail-name">{product.title}</h3>
-        <h4>{product.price}</h4>
-        <img src={ product.thumbnail } alt="" />
-        <button
-          id={ product.id }
-          type="button"
-          data-testid="product-add-to-cart"
-          onClick={ addToCart }
-        >
-          add
-        </button>
-      </section>
+      <div>
+        <Link to="/cart" data-testid="shopping-cart-button">
+          cart
+        </Link>
+        <section>
+          <h3 data-testid="product-detail-name">{product.title}</h3>
+          <h4>{product.price}</h4>
+          <img src={ product.thumbnail } alt="" />
+          <button
+            id={ product.id }
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            onClick={ addToCart }
+          >
+            add
+          </button>
+        </section>
+      </div>
     );
   }
 }
